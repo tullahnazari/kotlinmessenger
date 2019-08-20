794e5f3ccc50dcb8015a22fb81e9f932d53c2327
Mr. Tee <tullah.nazari@gmail.com>
2019-08-20 06:38:17 -0400

    Main activity and login activity
    - created ui for main activity
    - added login activity
    - create functionality to startlogin activity on click

--- src/main/java/com/tullahnazari/kotlinmessenger/MainActivity.kt
+++ src/main/java/com/tullahnazari/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.tullahnazari.kotlinmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.PasswordAuthentication
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //when users click register, this is what they will see
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity", "Email is: " + email)
+            Log.d("MainActivity", "Password $password")
+
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+
+            //launch login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
